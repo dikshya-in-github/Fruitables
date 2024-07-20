3a190f73b6a75ed50392b2730943ae912ebf6fea
dikshya-in-github <ghisingdeeku@email.com>
2024-07-20 14:58:18 +0545

    feat: react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import Navbar from "./components/Navbar"
+import ModalSearch from "./components/ModalSearch"
+import HeroStart from "./components/HeroStart"
+import FeatureSection from "./components/FeatureSection"
+import FruitShop from "./components/FruitShop"
+import Features from "./components/Features"
+import Vesitable from "./components/Vesitable"
+import Banner from "./components/Banner"
+import BestSeller from "./components/BestSeller"
+import Fact from "./components/Fact"
+import Tastimonial from "./components/Tastimonial"
 
 // Libraries Stylesheet
 import "./lib/lightbox/css/lightbox.min.css"
@@ -11,6 +21,16 @@ function App() {
     return (
         <>
             <Navbar />
+            <ModalSearch/>
+            <HeroStart/>
+            <FeatureSection/>
+            <FruitShop/>
+            <Features/>
+            <Vesitable/>
+            <Banner/>
+            <BestSeller/>
+            <Fact/>
+            <Tastimonial/>
         </>
     )
 }
